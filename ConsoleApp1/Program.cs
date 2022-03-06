c55a092beadb6e0da56c478e8b9e280f092b1134
Szymon Skorupka <s18295@pjwstk.edu.pl>
2022-03-06 13:56:00 +0100

    first

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+          
+            var websiteUrl = args[0];
+
+            HttpClient http = new HttpClient();
+            var response = await http.GetAsync(websiteUrl);
+            var content = await response.Content.ReadAsStringAsync();
+            var regex = new Regex(@"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+");
+
+            var matchCollection = regex.Matches(content);
+
+            foreach(var item in matchCollection)
+            {
+                Console.WriteLine(item);
+              
+            }
+            
+            //Console.WriteLine(response);
+        
+        
+        }
+    }
+}
