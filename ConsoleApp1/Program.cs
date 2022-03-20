2da68a7292247394e9119f81ca1d7bb9118f7bf1
Szymon Skorupka <s18295@pjwstk.edu.pl>
2022-03-20 10:14:07 +0100

    Zjazd1 praca domowa

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,25 +10,57 @@ namespace ConsoleApp1
     {
         public static async Task Main(string[] args)
         {
-          
-            var websiteUrl = args[0];
-
-            HttpClient http = new HttpClient();
-            var response = await http.GetAsync(websiteUrl);
-            var content = await response.Content.ReadAsStringAsync();
-            var regex = new Regex(@"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+");
-
-            var matchCollection = regex.Matches(content);
-
-            foreach(var item in matchCollection)
+            if (string.IsNullOrEmpty(args[0]))
             {
-                Console.WriteLine(item);
-              
+                throw new ArgumentNullException();
             }
-            
-            //Console.WriteLine(response);
-        
-        
+
+            var ulr = new UriBuilder(args[0]).Uri;
+
+            if (!Uri.IsWellFormedUriString(ulr.ToString(), UriKind.Absolute))
+            {
+                throw new ArgumentException();
+            }
+
+            await Program.GetWebPage(ulr.ToString());
+        }
+        public static async Task GetWebPage(String url)
+        {
+            using var httpClient = new HttpClient();
+            String pageSource = "";
+
+            try
+            {
+                pageSource = await httpClient.GetStringAsync(url);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Błąd wczasie pobierania strony");
+                Console.WriteLine(error);
+                return;
+            }
+
+
+            var emailRegex = @"(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+                             + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+                             + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                             + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})";
+
+            var matchResult = Regex
+                .Matches(pageSource, emailRegex)
+                .Select(match => match.Value)
+                .Distinct();
+
+            if (!matchResult.Any())
+            {
+                Console.WriteLine("Nie znaleziono emaili");
+                return;
+            }
+
+            foreach (var email in matchResult)
+            {
+                Console.WriteLine(email);
+            }
         }
     }
 }
